db6e992d7780b9dc18902332e8819ca77ae71817
Cíntia Fumi <cintiafumi@gmail.com>
2020-12-06 20:28:13 -0300

    chore(src): folder structure

---
+++ src/pages/home/index.js
@@ -0,0 +1,8 @@
+export const Home = () => {
+  // Coloque sua página
+  const title = `
+      <h1>Olá, mundo!</h1>
+  `;
+
+  return title;
+};
---
+++ src/router.js
@@ -0,0 +1,5 @@
+// Este é seu ponto de entrada da sua aplicação
+import { Home } from './pages/home/index.js';
+
+const root = document.getElementById('root');
+root.innerHTML = Home();
