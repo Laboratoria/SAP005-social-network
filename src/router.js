b4d243463ba74e4bee452ea91d92ca68ec93c411
Nayara.Fugii <nayarac.neto@gmail.com>
2020-12-14 19:31:40 -0300

    Inclusão de condições obrigatorias do cpf e data de nascimento

--- src/router.js
+++ src/router.js
@@ -1,48 +1,21 @@
 const cpf = document.querySelector('#cpf')
+const date = document.querySelector('#month')
 const btn = document.querySelector('#btn')
 
 btn.addEventListener("click", (e) => {
     e.preventDefault();
+    let dateBorned = date.value;
     let typedCpf = cpf.value;
-    let replaceCpf = typedCpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/,
-        (regex, argument1, argument2, argument3, argument4) => {
-            return argument1 + '.' + argument2 + '.' + argument3 + '-' + argument4;
-        })
-    console.log(typedCpf = replaceCpf);
+    if (typedCpf == "" || typedCpf.length < 11 || dateBorned == "") {
+        alert('teste')
+    } else {
+        let replaceCpf = typedCpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/,
+            (regex, argument1, argument2, argument3, argument4) => {
+                return argument1 + '.' + argument2 + '.' + argument3 + '-' + argument4;
+            })
+        console.log(typedCpf = replaceCpf, dateBorned);
+    }
+
+    cpf.value = "";
+    date.value = "";
 })
-
-
-// Este é seu ponto de entrada da sua aplicação
-// import { Home } from './pages/home/index.js';
-// import { Login } from './pages/login/index.js';
-// import { onNavigate } from './utils/history.js';
-
-// const routeRender = () => {
-//   const rootDiv = document.getElementById('root');
-//   const routes = {
-//     '/' : Home,
-//     '/login': Login,
-
-//   };
-
-//   rootDiv.innerHTML = '';
-//   rootDiv.appendChild(routes[window.location.pathname]());
-// };
-
-// window.addEventListener('popstate', routeRender);
-// window.addEventListener('load', () => {
-//   document
-//     .getElementById('home')
-//     .addEventListener('click', (e) => {
-//       e.preventDefault();
-//       onNavigate('/')
-//     });
-//   document
-//     .getElementById('login')
-//     .addEventListener('click', (e) => {
-//       e.preventDefault();
-//       onNavigate('/login')
-//     });
-
-//   routeRender();
-// });
