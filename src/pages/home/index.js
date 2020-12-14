a04c6a3bb1338d069838dc80552405e1c59380f7
Juliana <juliana.souzas@yahoo.com.br>
2020-12-14 17:00:16 -0300

    prototipo baixa fidelidade

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,17 @@ export const Home = () => {
   // Coloque sua página
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
-      <h1>Olá, mundo!</h1>
+      <header>
+        <h1>Olá, Nome</h1>
+      </header>
+      <main class="main-home">
+        <section class="post-enviar">
+          <textarea class="post" placeholder="Conte sobre sua última cerveja..."></textarea>
+          <button>Postar</button>
+        </section>
+        <section class="posts-enviados">
+        </section>
+      </main>
   `;
   return rootElement;
 };
