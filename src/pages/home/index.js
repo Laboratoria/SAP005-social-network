183201aa089ebfc30486d21a078a662254ebb419
Lu-Pereira <luci.lv14@gmail.com>
2020-12-14 23:36:00 -0300

    Estrutura do login modificada e css mobile.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,15 @@
 export const Home = () => {
-  // Coloque sua página
-  // const rootElement = document.createElement('div');
-  // rootElement.innerHTML = `
-  //     <h1>Olá, mundo!</h1>
-  // `;
-  // return rootElement;
+  const container = document.createElement('div');
+  container.classList.add('div-home');
+  container.innerHTML = `
+  <header>
+  <h1>Meu Perfil</h1>
+  </header>
+  <div>
+  <img src="">
+  <h2>$</h2>
+  <h3>$</h3>
+  </div>
+`;
+  return container;
 };
