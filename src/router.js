2533edc9aae6423a26663b10f4913d4117fe4205
Priscila <priscilassilveira97@gmail.com>
2020-12-15 16:19:47 -0300

    senha

--- src/router.js
+++ src/router.js
@@ -12,6 +12,51 @@ btn.addEventListener("click", (e) => {
 })
 
 
+
+
+const senha = document.querySelector("#senha");
+const confirmeSenha = document.querySelector("#confirmeSenha");
+const mensagemAlerta = document.querySelector(".mensagemAlerta");
+const verBtn = document.querySelector(".ver");
+const botao = document.querySelector("button");
+
+
+botao.onclick = function () {
+  if (senha.value != confirmeSenha.value) {
+    mensagemAlerta.style.display = "block";
+    mensagemAlerta.classList.remove("senhasCorretas");
+    mensagemAlerta.textContent = "Senhas não conferem";
+    return false;
+  } else {
+    mensagemAlerta.style.display = "block";
+    mensagemAlerta.classList.add("senhasCorretas");
+    mensagemAlerta.textContent = "senhas conferem";
+    return false;
+  }
+}
+function confirmarSenhas() {
+  if (confirmeSenha.value != "") {
+    verBtn.style.display = "block";
+    verBtn.onclick = function () {
+      if ((senha.type == "password") && (confirmeSenha.type == "password")) {
+        senha.type = "text";
+        confirmeSenha.type = "text";
+        this.textContent = "Não ver";
+      } else {
+        senha.type = "password";
+        confirmeSenha.type = "password";
+        this.textContent = "ver";  
+      }
+    }
+  } else {
+    verBtn.style.display = "none";
+  }
+}
+
+
+
+
+
 // Este é seu ponto de entrada da sua aplicação
 // import { Home } from './pages/home/index.js';
 // import { Login } from './pages/login/index.js';
