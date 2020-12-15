96f9d4fecfc03dc0629d8c05d6077eed559d313b
alencartha <alencar_tha@hotmail.com>
2020-12-14 22:26:25 -0300

    organização de pastas e importação de arquivos css e js para o rooter.js

---
+++ src/pages/publicacoes/index.js
@@ -0,0 +1,9 @@
+export const publicacoes = () => {
+    // Coloque sua página
+    const rootElement = document.createElement('div');
+    rootElement.innerHTML = `
+        <h1>Visualizar publicações!</h1>
+    `;
+    return rootElement;
+  };
+  
---
+++ src/pages/publicar/index.spec.js
@@ -0,0 +1,7 @@
+import { publicar } from './index.js';
+
+describe('Publicar', () => {
+  it('should be a function', () => {
+    expect(typeof publicar).toBe('function');
+  });
+});
