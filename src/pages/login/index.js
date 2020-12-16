4732e05d2b9fb538072585e52fb30462477a82fc
Mairã Soares <71467930+MairaSoares@users.noreply.github.com>
2020-12-16 20:42:23 -0300

    inicio dos trabalhos com o login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,7 +2,18 @@ export const Login = () => {
   // Coloque sua página
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
-      <h1>Login!</h1>
+      <header class="login-header">
+      <nav class="login-inputs">
+         <ul>
+            <a href="#"> <li> <form action=""> <input id="user-in" class="input-login" type="text" placeholder="Email"> </form> </li> </a>
+            <a href="#"> <li> <form action=""> <input id="password-in" class="input-login" type="password" placeholder="Senha"> </form> </li> </a>
+            <a href="#"> <li> <button id="button-login" class="button-login">Entrar</button> </li> </a>
+            <p>OU</p>
+            <a href="#"> <li> <button class="google-login">Continuar com uma conta GOOGLE</button> </li> </a>
+            <a class="link-cadastro" href="./subscription.html"> <p> Criar uma conta </p> </a>
+         </ul>
+      </nav>
+   </header>
   `;
   return rootElement;
 };
