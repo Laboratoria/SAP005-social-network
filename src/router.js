0289d8f6860adede53f761f6ab2800c4a0f96cee
Priscila <priscilassilveira97@gmail.com>
2020-12-17 12:53:40 -0300

    inputVermelho

--- src/router.js
+++ src/router.js
@@ -19,9 +19,13 @@ btn.addEventListener("click", (e) => {
     let dateBorned = date.value;
     let typedCpf = cpf.value;
 
-    if (typedCpf == "" || typedCpf.length < 11 || dateBorned == "" || passwordFirst != passwordSecond || userName == "" || lastNameUser == "") {
-        alert('teste')
-    } else {
+    if (typedCpf == "" || typedCpf.length < 11 || dateBorned == "" || userName == "" || lastNameUser == "") {
+        alert('teste');
+    } else if(passwordFirst != passwordSecond ){
+        confirmPassword.style.borderColor = "red";
+    }else if(passwordFirst == passwordSecond){
+        confirmPassword.style.borderColor = "none";
+    }else {
         let replaceCpf = typedCpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, 
             (regex, argument1, argument2, argument3, argument4) => {
                 return argument1 + '.' + argument2 + '.' + argument3 + '-' + argument4;
