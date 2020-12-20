00480b867ed085fdb11ddc5fb261ca144988c4d2
Beatriz Penalva <biapenalva@gmail.com>
2020-12-20 14:24:42 -0300

    estrutura da página de login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,17 @@
-import { getUsers } from '../../services/index.js'
-const users = getUsers();
+/*
+const email = "teste@teste.com"
+const password = "chocolate"
+
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then(() => {
+    console.log("hello")
+  })
+  .catch((error) => {
+    console.log("deu ruim")
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+*/
 export const Login = () => {
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
@@ -11,50 +23,69 @@ export const Login = () => {
       <section class="right">  
         <input type="email" id="email" class="input-in-line" placeholder="E-mail">
         <input type="password" id="password" class="input-in-line" placeholder="Password">
-        <button class="enter-button" id="login">Entrar</button>
+        <button class="enter-button" id="login">Login</button> <button class="enter-button" id="singUp">Sing Up</button>
         <p class="subtitle">_______________ OU _______________</p>
         <section class="login-social-media">
-          <button id="google" class="input-icon"><img src='../../img/google.svg' height="50px" width="50px" alt="Logo Google na cor laranja"></button>
-          <button id="facebook" class="input-icon"><img src='../../img/facebook.svg' height="50px" width="50px" alt="Logo Facebook na cor laranja"></button>
-          <button id="github" class="input-icon"><img src='../../img/github.svg' height="50px" width="50px" alt="Logo GitHub na cor laranja"></button>
+          <button id="google" class="button-icon"><img src='../../img/google.svg' height="50px" width="50px" alt="Logo Google na cor laranja"></button>
+          <button id="facebook" class="button-icon"><img src='../../img/facebook.svg' height="50px" width="50px" alt="Logo Facebook na cor laranja"></button>
+          <button id="github" class="button-icon"><img src='../../img/github.svg' height="50px" width="50px" alt="Logo GitHub na cor laranja"></button>
         </section>  
         <p class="subtitle">Não tem uma conta?
-        <button id="createAccount" class="input-in-text">Registre-se</button>
+        <button id="createAccount" class="button-text">Registre-se</button>
         </p>
       </section>
     </section>    
   `;
   const singIn = rootElement.querySelector('#login');
   const singUp = rootElement.querySelector('#createAccount');
+  const singUpGoogle = rootElement.querySelector('#google');
+  const singUpFb = rootElement.querySelector('#facebook');
+  const singUpGh = rootElement.querySelector('#github');
   singIn.addEventListener("mouseenter", () => {
     singIn.style.background = '#FEBB86';
   });
   singIn.addEventListener("mouseout", () => {
     singIn.style.background = '#FF780F';
   });
-  singIn.addEventListener("click", () => {
+  singUp.addEventListener("mouseenter", () => {
+    singUp.style.textDecoration = 'underline';
+  });
+  singUp.addEventListener("mouseout", () => {
+    singUp.style.textDecoration = 'none';
+  });
+  singIn.addEventListener("click", e => {
     singIn.style.background = '#FEBB86';
     const email = rootElement.querySelector("#email").value;
     const password = rootElement.querySelector("#password").value;
-    const found = users.find(element => element.email === email);
-    if (found !== undefined && password === found.password) {
-      console.log("Bem-vinda")
-    //routeRender('/');  window.location.'home'
-    } else {
-      console.log("E-mail ou senha inválidos")
-    //printar essa mensagem na tela
-    }
-  });
-  rootElement.querySelector("#google").addEventListener("click", loginGoogle);
-  rootElement.querySelector("#facebook").addEventListener("click", loginFacebook);
-  rootElement.querySelector("#github").addEventListener("click", loginGitHub);
+    const auth = firebase.auth();
+    //vai retornar uma promessa e dá pra trabalhar de maneira assíncrona
+    const promise = auth.signInWithEmailAndPassword(email, password);
+    promise.catch(e => console.log (e.message));
 
+
+    auth.createUsernWithEmailAndPassword(email, password);
+    auth.onAuthStateChange (firebaseUser => {});
+
+
+
+  });
+  singUpGoogle.addEventListener("click", loginGoogle);
+  singUpFb.addEventListener("click", loginFacebook);
+  singUpGh.addEventListener("click", loginGitHub);
   singUp.addEventListener("click", () => {
     console.log('create an account');
-    //onNavigate('/register)
   });
   return rootElement;
 };
+
+
+
+
+
+
+
+
+
 function loginGoogle() {
   console.log('google');
 }
@@ -64,10 +95,14 @@ function loginFacebook() {
 function loginGitHub() {
   console.log('github');
 }
+
+
+
+
   /*
   Etapas importantes:
-  >>>Mudar a rota das páginas<<<
   >>>Autentificação com Firebase<<<
+  >>>Mudar a rota das páginas<<<
   >>>Testes unitários<<<
   
   Dúvidas:
@@ -76,14 +111,7 @@ function loginGitHub() {
   
   UI - UX:
   Opção de deixar vísivel a senha (com um checkbox)
-  Mandar mensagem de campo vazio | oninvalid="alert("Por favor, preencha o campo!');" required | quando fizer o formulário /  required oninvalid="setCustomValidity('Por favor, preencha o nome')" / <input type="text" name="txtnome" required oninvalid="setCustomValidity('Por favor, preencha o nome')" onchange="try{setCustomValidity('')}catch(e){}" />
-  Efeitos de Mouse quando clica nos inputs de texto e no botão
+  Input e-mail tá ficando azul
+  Mandar mensagem de campo vazio
   Acessibilidade
   */
-
-//função de printar na tela
-
-/*função de pegar o valor da tela
-pega o valor, crio uma constante chamando a função que vai fazer a mudança, passo os parâmetros
-passo a função de printar com o pâmetro do resultado da constante
-*/
