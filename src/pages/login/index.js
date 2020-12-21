7ebbd2eafdc3886bcbd2707a82f7806a5635342a
Tânagra Andria <design.andria@gmail.com>
2020-12-21 12:45:04 -0300

    Botões respondendo

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -11,30 +11,50 @@ export const Login = () => {
     <div id="root">
      <h4>Your Pet's ally !!!</h4>
     </div>
-  
-    <section class="email">
+    <section id="form">
+    <div class="email">
   
     <h3> <Label>E-mail:</Label>
       <input type="text" id="e-mail">
     </h3> 
-  </section>
+  </div>
 
-  <section class="password">
+  <div class="password">
     <h3>
       <Label>Senha:</Label>
       <input type="password" id="password"> 
     </h3> 
+    </div>
   </section>
 
   <section class"buttons">
     <h3>
-      <button class="register" onclick="register ()">Cadastrar</button>
-      <button class="login" onclick="login ()">Login</button>
+      <button id="register" class="register">Cadastrar</button>
+      <button id="login" class="login">Login</button>
     </h3>
   </section>
   </div> 
 
   `;
+const registerButton= rootElement.querySelector("#register")
+const loginButton= rootElement.querySelector("#login")
+
+    registerButton.addEventListener( "click",  () => {
+    console.log ("oi")
+    
+    
+        // alert("Cadastrado com sucesso");
+         
+      }); 
+    
+    
+
+    loginButton.addEventListener( "click",   () => {   
+      console.log ("sim")  
+      
+    }) 
+
+    
   return rootElement;
 };
 
