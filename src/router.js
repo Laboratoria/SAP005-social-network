7d321f45347e3ebc69fc17ecc8e3395eecff0183
Nayara.Fugii <nayarac.neto@gmail.com>
2020-12-21 17:48:29 -0300

    inclusão de função do cadastro

--- src/router.js
+++ src/router.js
@@ -2,22 +2,46 @@ import { Register } from './pages/register/index.js';
 import { Login } from './pages/login/index.js';
 import { onNavigate } from './utils/history.js';
 
-const root = document.querySelector("#root");
 
-const routers = {
-    "/": Login,
-    "/cadastro": Register
-}
+const routeRender = () => {
+    const rootDiv = document.getElementById('root');
+    const routes = {
+        "/": Login,
+        "/cadastro": Register
 
-const renderRouter = () => {
-    root.innerHTML = "";
-    if (!routers[window.location.pathname]) {
-        window.location = '/'
-    } else {
-        root.appendChild(routers[window.location.pathname]());
-    }
-}
+    };
 
-document.addEventListener('DOMContentLoaded', () => {
-    renderRouter();
-})
+    rootDiv.innerHTML = '';
+    rootDiv.appendChild(routes[window.location.pathname]());
+};
+
+window.addEventListener('popstate', routeRender);
+window.addEventListener('load', () => {
+    // document
+    //     .getElementById('home')
+    //     .addEventListener('click', (e) => {
+    //         e.preventDefault();
+    //         onNavigate('/')
+    //     });
+    // document
+    //     .getElementById('login')
+    //     .addEventListener('click', (e) => {
+    //         e.preventDefault();
+    //         onNavigate('/login')
+    //     });
+
+    routeRender();
+});
+
+
+// const renderRouter = () => {
+//     root.innerHTML = "";
+//     if (!routers[window.location.pathname]) {
+//         window.location = '/'
+//     } else {
+//         root.appendChild(routers[window.location.pathname]());
+//     }
+// }
+// document.addEventListener('DOMContentLoaded', () => {
+//     renderRouter();
+// })
