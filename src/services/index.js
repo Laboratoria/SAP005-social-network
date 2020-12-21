4500a2b8f5f35ee13e9a07824c0125e11f75726f
apretamayara <mayara.oliveirapessoal@gmail.com>
2020-12-21 14:05:35 -0300

    cadastroOk

--- src/services/index.js
+++ src/services/index.js
@@ -2,5 +2,15 @@
 
 export const myFunction = () => {
   // seu código aqui
-  console.log('Olá mundo!');
+  console.log();
 };
+
+let form = document.getElementById("register-form");
+let submit = document.getElementById("btn-submit");
+
+submit.addEventListener('click', function(e){
+
+  e.preventDefault();
+  console.log('funcionou');
+  console.log('oi');
+}
