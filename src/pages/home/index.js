fb6faddab807be0bfd6a9b29b5b4392b3eaab742
JulianaAmoriN <juamorim.sn@gmail.com>
2020-12-22 11:26:22 -0300

    sobre text

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,35 @@ export const Home = () => {
   // Coloque sua página
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
+      <img>
       <h1>Sobre</h1>
+      <h1>Entrar</h1>
+
+      <div>
+         <figure>
+            <img>
+            <img>
+            <img>
+        </figure>
+        <p></p>
+      </div>
+
+      <div>
+         <figure>
+           <img>
+         </figure>
+         <p>
+         Olimpo é um aplicativo direcionado a pessoas que praticam esporte, tem como objetivo ser um espaço para troca de informações, novas amizades e compartilhamento dicas. 
+         Faça parte, encontre amigos que assim como você amam praticar esportes.
+         <br>
+         Esse projeto foi desenvolvido por: 
+         </p>
+         <figure>
+            <P><img>Giovana</P>
+            <P><img>Juliana</P>
+            <P><img>Hadassa</P>
+        </figure>
+      </div>
       `;
   return rootElement;
 };
