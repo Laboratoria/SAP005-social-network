4d503e86daa6fada82982db621d207e628e1e1a5
akemimeka <akemimeka@gmail.com>
2020-12-21 21:37:10 -0300

    Change some classes to ids and refactor figcaption tag to only include the quote author's name

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -5,15 +5,15 @@ export const Profile = () => {
 
   profileBody.innerHTML = `
     <header class="profile-header">
-      <div class="profile-banner"></div>
+      <div id="profile-banner">IMG-BANNER</div>
       <div class="profile-user-info">
-        <h2 class="profile-user-name">Samara Tokki</h2>
+        <h2 id="profile-user-name">Samara Tokki</h2>
         <figure class="profile-favorite-quote">
-          <blockquote class="profile-quote-text">
+          <blockquote id="profile-quote-text">
             Não confio em pessoas que não se amam e mesmo assim dizem "eu te amo". Eis o que diz um provérbio africano: tome cuidado quando uma pessoa nua lhe oferece uma camisa.
           </blockquote>
-          <figcaption class="profile-quote-author">
-            &mdash; Maya Angelou, <cite>palestra na Weber State University</cite>
+          <figcaption id="profile-quote-author">
+            &mdash; Maya Angelou
           </figcaption>
         </figure>
       </div>
