1fb14e35ab0975e8873729c967402be6a1df56c6
Tânagra Andria <design.andria@gmail.com>
2020-12-22 16:28:17 -0300

    Função dos botões dialogando com o firebase

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -4,50 +4,51 @@ export const Login = () => {
   rootElement.innerHTML = `
   <div  id="firebaseui-auth-container" class="container-login">
   
-    <img src="./img/logo.png" class="logo">
-    <img src="./img/pessoas.png" class="people">
-  
-  
-    <div id="root">
-     <h4>Your Pet's ally !!!</h4>
-    </div>
-    <section id="form">
-    <div class="email">
-  
-    <h3> <Label>E-mail:</Label>
-      <input type="text" id="e-mail">
-    </h3> 
-  </div>
+          <img src="./img/logo.png" class="logo">
+          <img src="./img/pessoas.png" class="people">
+        
+        
+          <div id="root">
+            <h4>Your Pet's ally !!!</h4>
+          </div>
 
-  <div class="password">
-    <h3>
-      <Label>Senha:</Label>
-      <input type="password" id="password"> 
-    </h3> 
-    </div>
-  </section>
+          
+          <div class="form" id="form">
+        
+            <h3> <Label>E-mail:</Label>
+              <input type="text" id="e-mail">
+            </h3> 
+        
+            <h3>
+              <Label>Senha:</Label>
+              <input type="password" id="password"> 
+            </h3> 
 
-  <section class"buttons">
-    <h3>
-      <button id="register" class="register">Cadastrar</button>
-      <button id="login" class="login">Login</button>
-    </h3>
-  </section>
-  <div class = "g-signin2" data-onsuccess = "onSignIn" data-theme = "dark" > </div>       
+          </div>
+
+        <div class"buttons">
+          <h3>
+            <button id="register" class="register">Cadastrar</button>
+            <button id="login" class="login">Login</button>
+          </h3>
+        </div>
+
+        <div id= "google" class= "g-signin2" data-onsuccess= "onSignIn" data-theme= "dark" > </div>       
   </div> 
 
   `;
 
-  const emailButton= rootElement.querySelector("#e-mail")  
-  const passwordButton= rootElement.querySelector("#password") 
-  const registerButton= rootElement.querySelector("#register")
-  const loginButton= rootElement.querySelector("#login")
+  const emailButton = rootElement.querySelector("#e-mail")
+  const passwordButton = rootElement.querySelector("#password")
+  const registerButton = rootElement.querySelector("#register")
+  const loginButton = rootElement.querySelector("#login")
+  const googleButton = rootElement.querySelector("#google")
 
-    registerButton.addEventListener( "click",  () => {
-    console.log ("oi")
-    firebase.auth().createUserWithEmailAndPassword(emailButton.value ,passwordButton.value )
-    console.log (emailButton.value)
-    console.log (passwordButton.value)
+  registerButton.addEventListener("click", () => {
+    console.log("oi")
+    firebase.auth().createUserWithEmailAndPassword(emailButton.value, passwordButton.value)
+    console.log(emailButton.value)
+    console.log(passwordButton.value)
     // then(()=> console.log("deu certo")) 
     //     alert("Cadastrado com sucesso")   
     // .catch((error)=> {
@@ -55,16 +56,16 @@ export const Login = () => {
     //       var errorMessage = error.message;
     //       // ..
     // });  .
-   
-      
-    }); 
-    loginButton.addEventListener( "click",   () => {   
- 
-     console.log ("sim")        
-    }) 
 
-  
-    
-    
-  return rootElement;
-};
+  });
+  loginButton.addEventListener("click", () => {
+    console.log("sim")
+  })
+
+    googleButton.addEventListener("click", () => {
+      
+    })
+
+
+        return rootElement;
+ };
--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 // Este é seu ponto de entrada da sua aplicação
 import { Home } from './pages/home/index.js';
-import { Login } from './pages/login/index.js';
-import {Login} from './pages/login/auth.js';
+import  {Login} from './pages/login/index.js';
 import { onNavigate } from './utils/history.js';
 
 const routeRender = () => {
