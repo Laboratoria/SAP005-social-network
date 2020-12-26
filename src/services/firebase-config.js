9db04c5b6dfae1fbf0063667614c862a8809d2b8
akemimeka <akemimeka@gmail.com>
2020-12-26 17:24:51 -0300

    Formats firebase-config.js according to Prettier extension

--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCOgmjN4FdCSRmmXs0Y8W6GxQPlFgZTB5w",
-  authDomain: "social-network-trio.firebaseapp.com",
-  projectId: "social-network-trio",
-  storageBucket: "social-network-trio.appspot.com",
-  messagingSenderId: "1082190625907",
-  appId: "1:1082190625907:web:c27c01bdfa7ab07a857e06"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: 'AIzaSyCOgmjN4FdCSRmmXs0Y8W6GxQPlFgZTB5w',
+  authDomain: 'social-network-trio.firebaseapp.com',
+  projectId: 'social-network-trio',
+  storageBucket: 'social-network-trio.appspot.com',
+  messagingSenderId: '1082190625907',
+  appId: '1:1082190625907:web:c27c01bdfa7ab07a857e06',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
