c51c1aa1316a6a9a1514fa2bc22c138c1c750591
akemimeka <akemimeka@gmail.com>
2020-12-26 14:55:03 -0300

    Changes <main> to <header> and adds class profile-header

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -3,26 +3,25 @@ import { Post } from '../../components/post/index.js';
 
 export const Profile = () => {
   const profileBody = document.querySelector('#root');
-  const profileMain = document.createElement('main');
-  profileMain.className = 'profile-main';
+  const profileHeader = document.createElement('header');
+  profileHeader.className = 'profile-header';
 
-  profileMain.innerHTML = `
-    <header class="profile-header">
-      <div id="profile-banner">IMG-BANNER</div>
-      <div class="profile-user-info">
-        <h2 id="profile-user-name">Samara Tokki</h2>
-        <figure class="profile-favorite-quote">
-          <blockquote id="profile-quote-text">
-            Não confio em pessoas que não se amam e mesmo assim dizem "eu te amo". Eis o que diz um provérbio africano: tome cuidado quando uma pessoa nua lhe oferece uma camisa.
-          </blockquote>
-          <figcaption id="profile-quote-author">
-            &mdash; Maya Angelou
-          </figcaption>
-        </figure>
-      </div>
-    </header>
+  profileHeader.innerHTML = `
+    <div id="profile-banner">IMG-BANNER</div>
+    <div class="profile-user-info">
+      <h2 id="profile-user-name">Samara Tokki</h2>
+      <figure class="profile-favorite-quote">
+        <blockquote id="profile-quote-text">
+          Não confio em pessoas que não se amam e mesmo assim dizem "eu te amo". Eis o que diz um provérbio africano: tome cuidado quando uma pessoa nua lhe oferece uma camisa.
+        </blockquote>
+        <figcaption id="profile-quote-author">
+          &mdash; Maya Angelou
+        </figcaption>
+      </figure>
+    </div>
   `;
 
+  profileBody.appendChild(profileHeader);
   profileBody.appendChild(Post());
   profileBody.appendChild(Navbar());
 
