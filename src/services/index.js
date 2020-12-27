6a83d3449f8e9cc548a49fe53a8d57a48c073358
akemimeka <akemimeka@gmail.com>
2020-12-26 23:28:08 -0300

    Comments the firebase test, creates the googleLogin function and exports it

--- src/services/index.js
+++ src/services/index.js
@@ -1,17 +1,24 @@
-// exporte suas funções
-const email = "teste1@teste.com"
-const password = "teste1234"
-firebase
-  .auth()
-  .createUserWithEmailAndPassword(email, password)
-  .then(() => console.log("deu certo"))
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log("deu ruim")
-  });
+// const email = 'teste1@teste.com';
+// const password = 'teste1234';
 
-export const myFunction = () => {
-  // seu código aqui
-  console.log('Olá mundo!');
+// firebase
+//   .auth()
+//   .createUserWithEmailAndPassword(email, password)
+//   .then(() => console.log('deu certo'))
+//   .catch((error) => {
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     console.log('deu ruim');
+//   });
+
+export const googleLogin = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider)
+    .then((result) => {
+      const token = result.credential.accessToken;
+      const user = result.user;
+      console.log(user, token);
+    })
+    .catch();
 };
