42a30b51a575d054afa31594c02960399c1333bc
Nayara.Fugii <nayarac.neto@gmail.com>
2020-12-29 18:19:04 -0300

    login com o google

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -63,7 +63,27 @@ export const Login = () => {
         passwordInput.value = ""
     })
 
+    rootElement.querySelector("#google").addEventListener("click", (e) => {
+        e.preventDefault()
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            // var token = result.credential.accessToken;
+            // The signed-in user info.
+            // var user = result.user;
+            console.log(result)
+            onNavigate("/home")
+        }).catch(function(error) {
+            // Handle Errors here.
+            // var errorCode = error.code;
+            // var errorMessage = error.message;
+            // The email of the user's account used.
+            // var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            // var credential = error.credential;
+            // ...
+        });
+
+    });
     return rootElement;
-
-
 };
