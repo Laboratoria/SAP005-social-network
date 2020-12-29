3e120c4d524c0332ccf5b61ac118eea2f65a2267
Nijoane <nicajoane@outlook.com>
2020-12-29 15:45:50 -0300

    arquivos não alterados

--- src/pages/singup/index.js
+++ src/pages/singup/index.js
@@ -3,16 +3,17 @@ import { CreateUser } from '../../services/index.js';
 export const SingUp = () => {
   // Coloque sua página
   const rootElement = document.createElement('div');
-  rootElement.innerHTML = `
-    <h1>Sing up!</h1>
-    <form class="singup-form">
-      <input class="name" type="text" placeholder="Name">
-      <input class="email" type="email" placeholder="E-mail">
-      <input class="password" type="password" placeholder="Password">
-      <input class="confirm-password" type="password" placeholder="Confirm Password">
 
-      <button class="btn-create">Create your account</button>
-    </form>
+  rootElement.innerHTML = `
+      <h1>Sing up!</h1>
+      <form class="singup-form">
+        <input class="name" type="text" placeholder="Name">
+        <input class="email" type="email" placeholder="E-mail">
+        <input class="password" type="password" placeholder="Password">
+        <input class="confirm-password" type="password" placeholder="Confirm Password">
+
+        <button class="btn-create">Create your account</button>
+      </form>
     `;
   return rootElement;
 };
