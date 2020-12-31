e6097eecf0fad71bcf6189d08d695aef772c5490
Beatriz Penalva <biapenalva@gmail.com>
2020-12-31 12:27:19 -0300

    tratamento de erros

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -8,14 +8,14 @@ export const Login = () => {
         <img src='../../img/ada-lovelace.svg' width="175px" height="175px" alt="Desenho do rosto de Ada Lovelace em preto com um fundo redondo alaranjado">
         <p class="theme"><span class="logoname">[Ada]</span> Programe como uma mulher.</p>
       </section>
-      <section class="right">  
-        <input type="email" id="email" class="input-in-line" placeholder="E-mail">
-        <input type="password" id="password" class="input-in-line" placeholder="Password">
+      <section class="right">
+        <input type="email" id="email" class="input-in-line" placeholder="E-mail" required>
+        <input type="password" id="password" class="input-in-line" placeholder="Password" required>
         <section id="errorLogin" class="errorMessage"></section> 
         <section id="login-button"">
           <button class="enter-button" id="signIn">Sign In</button> 
           <button class="enter-button" id="signUp">Sign Up</button>
-        </section>  
+        </section>
         <p class="subtitle">_______________ OU _______________</p>
         <section class="login-button">
           <button id="google" class="button-icon"><img src='../../img/google.svg' height="50px" width="50px" alt="Logo Google na cor laranja"></button>
@@ -35,87 +35,102 @@ export const Login = () => {
   const signUpGoogle = rootElement.querySelector('#google');
   const signUpFb = rootElement.querySelector('#facebook');
   const signUpGh = rootElement.querySelector('#github');
-  //------------------ EVENTOS DE MOUSE PARA USABILIDADE  ------------------\\
-  signIn.addEventListener("mouseenter", () => {
-    signIn.style.background = '#FEBB86';
-  });
-  signIn.addEventListener("mouseout", () => {
-    signIn.style.background = '#FF780F';
-  });
-  signUp.addEventListener("mouseenter", () => {
-    signUp.style.background = '#FEBB86';
-  });
-  signUp.addEventListener("mouseout", () => {
-    signUp.style.background = '#FF780F';
-  });
-  register.addEventListener("mouseenter", () => {
-    register.style.textDecoration = 'underline';
-  });
-  register.addEventListener("mouseout", () => {
-    register.style.textDecoration = 'none';
-  });
-  //--------------------- FUNÇÕES DE AUTENTIFICAÇÃO ----------------------\\
-  //-------------- TODO: Uso correto de erros para promessas --------------\\  
+  //------------------------- MENSAGENS DE ERRO ------------------------- \\
+  const verifyErrorCode = {
+    "auth/invalid-email": "O endereço de e-mail não é válido. Por favor, preencha novamente.",
+    "auth/invalid-password": "Senha incorreta. Por favor, tente novamente.",
+    "auth/email-already-in-use": "O e-mail fornecido já está cadastrado. Por favor, forneça um novo endereço.",
+    "auth/user-not-found": "Não há registro desse usuário. Por favor, registre-se para ter acesso à nossa rede.",
+    "auth/account-exists-with-different-credential": "E-mail já associado a outra conta. Por favor, tente com um novo endereço.",
+    "default": "Ocorreu algum erro. Por favor, tente novamente",
+  }
+  const errorMessageEmptyInput = "O preenchimento dos campos de e-mail e senha é obrigatório.";
+  //------------------------------- EVENTOS -------------------------------\\
+  //---------------------- FUNÇÕES DE AUTENTIFICAÇÃO ----------------------\\
   signIn.addEventListener("click", e => {
-    signIn.style.background = '#FEBB86';
     const email = rootElement.querySelector("#email").value;
     const password = rootElement.querySelector("#password").value;
-    const promise = firebase.auth().signInWithEmailAndPassword(email, password);
-    promise
-      .then(() => {
-        onNavigate('/');
-      })
-      .catch(() => {
-        const elementError = document.createElement("p");
-        const errorMessage = document.createTextNode(`E-mail ou senha inválida. Por favor, tente novamente.`);
-        elementError.appendChild(errorMessage);
-        document.getElementById("errorLogin").appendChild(elementError);
-      });
+    if (email === "" || password === "") {
+      printMessageError(errorMessageEmptyInput);
+    } else {
+      const promise = firebase.auth().signInWithEmailAndPassword(email, password);
+      promise
+        .then(() => {
+          onNavigate('/');
+        })
+        .catch(err => {
+          const errorCode = err.code;
+          let errorMessage = verifyErrorCode[errorCode];
+          if (errorMessage == null) {
+            errorMessage = err.Message;
+          }
+          printMessageError(errorMessage);
+        });
+    }
   });
+  //-------------- TODO: Validação de e-mail e não permitir usuários repetidos ---------------\\
   signUp.addEventListener("click", e => {
-    signUp.style.background = '#FEBB86';
     const email = rootElement.querySelector("#email").value;
     const password = rootElement.querySelector("#password").value;
-    const promise = firebase.auth().createUserWithEmailAndPassword(email, password);
-    promise
-      .then(() => {
-        onNavigate('/');
-      })
-      .catch(() => {
-        console.log("Deu ruim")
-      });
+    if (email === "" || password === "") {
+      printMessageError(errorMessageEmptyInput);
+    } else {
+      const promise = firebase.auth().createUserWithEmailAndPassword(email, password);
+      promise
+        .then(() => {
+          onNavigate('/');
+        }).catch(err => {
+          const errorCode = err.code;
+          let errorMessage = verifyErrorCode[errorCode];
+          if (errorMessage == null) {
+            errorMessage = err.Message;
+          }
+          printMessageError(errorMessage);
+        });
+    }
   });
   signUpGoogle.addEventListener("click", () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithRedirect(provider)
-      .then((result) => {
-        console.log("usuario", result.user);
-        console.log("token", result.credential.accessToken);
-      }).catch(() => {
-        console.log("Deu ruim")
+      .then(() => {
+        onNavigate('/');
+      }).catch(err => {
+        const errorCode = err.code;
+        let errorMessage = verifyErrorCode[errorCode];
+        if (errorMessage == null) {
+          errorMessage = err.Message;
+        }
+        printMessageError(errorMessage);
       });
   });
   signUpFb.addEventListener("click", () => {
     const provider = new firebase.auth.FacebookAuthProvider();
     firebase.auth().signInWithRedirect(provider)
-      .then((result) => {
-        console.log("usuario", result.user);
-        console.log("token", result.credential.accessToken);
-      }).catch(() => {
-        console.log("Deu ruim")
+      .then(() => {
+        onNavigate('/');
+      }).catch(err => {
+        const errorCode = err.code;
+        let errorMessage = verifyErrorCode[errorCode];
+        if (errorMessage == null) {
+          errorMessage = err.Message;
+        }
+        printMessageError(errorMessage);
       });
   });
   signUpGh.addEventListener("click", () => {
     const provider = new firebase.auth.GithubAuthProvider();
     firebase.auth().signInWithRedirect(provider)
-      .then((result) => {
-        console.log("usuario", result.user);
-        console.log("token", result.credential.accessToken);
-      }).catch(() => {
-        console.log("Deu ruim")
+      .then(() => {
+        onNavigate('/');
+      }).catch(err => {
+        const errorCode = err.code;
+        let errorMessage = verifyErrorCode[errorCode];
+        if (errorMessage == null) {
+          errorMessage = err.Message;
+        }
+        printMessageError(errorMessage);
       });
   });
-
   //---------- ENCAMINHAR PARA O FORMULÁRIO DE CRIAÇÃO DE PERFIL ----------\\
   //---------------- TODO: Mudar para a página de registro ----------------\\
   register.addEventListener("click", () => {
@@ -123,18 +138,10 @@ export const Login = () => {
   });
   return rootElement;
 };
-
-//---------------- AUTENTIFICAÇÃO DO USUÁRIO EM TEMPO REAL ----------------\\
-/*
-  firebase.auth().onAuthStateChange (firebaseUser => {});
-  if (firebaseUser) {
-    console.log(firebaseUser);
-  } else {
-    console.log("não está logando");
-  }
-*/
-
-//---------------- FUNÇÃO DE LOGOUT ----------------\\
-/*
-  firebase.auth().signOut
-*/
+function printMessageError(message) {
+  const elementError = document.createElement("p");
+  const errorMessage = document.createTextNode(message);
+  elementError.appendChild(errorMessage);
+  document.getElementById("errorLogin").innerHTML = "";
+  document.getElementById("errorLogin").appendChild(elementError);
+}
