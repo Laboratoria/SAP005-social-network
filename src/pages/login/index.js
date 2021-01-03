bca1063d65dc9bfdee40ba57abe2596a853a6adf
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2021-01-03 17:23:54 -0300

    Added html login mobile

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,8 +1,27 @@
 export const Login = () => {
-  // Coloque sua página
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
-      <h1>Login!</h1>
+  <div class="main-login">
+    <img src="/images/logo-deva.png">
+    <div class="mail-login">
+      <form>
+        <input type="text" placeholder="Email ou nome de usuária">
+        </input>
+        <input type="text" placeholder="Insira sua senha">
+        </input>
+        <button type="submit">LOGIN</button>
+      </form>
+    </div>
+    <h2>OU</h2>
+    <div class="google-github-login">
+      <button type="submit">Entrar com GOOGLE</button>
+      <button type="submit">Entrar com GITHUB</button>
+    </div>
+    <div class="new-register">
+      <p>Não tem uma conta?</p>
+      <a href="#">Cadastre-se</a>
+    </div>
+  </div>
   `;
   return rootElement;
 };
