c45c09ee09fa761b30202f5f67ed56fc489fd2ae
caroline costa <carolmarcionilia@gmail.com>
2021-01-03 19:26:26 -0300

    firebase authentication with email and password

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,3 +1,4 @@
+import { emailAndPasswordLogin } from '../../services/index.js';
 import { googleLogin } from '../../services/index.js';
 
 export const Login = () => {
@@ -9,13 +10,13 @@ export const Login = () => {
         <h2 class="title-login">Nome do app</h2>
       </header>
       <section>
-        <form class="form-login">
+        <form class="form-login" id="form>
         <label class="label-enter">Entrar</label>
         <label class="label-login">E-mail</label>
-        <input type="email" class="input-login" name="email-login" required>
+        <input type="email" class="input-login" id="email-login" name="email-login" required>
         <label class="label-login">Senha</label>
-        <input type="password" class="input-login" minlength="8" name="password-login" required>
-        <button type="submit" class="btn-login">Entrar</button>
+        <input type="password" class="input-login" id="password-login" minlength="8" name="password-login" required>
+        <button type="submit" class="btn-login" id="btn-login">Entrar</button>
         <label class="alternative">Ou</label>
         <button id="btn-google">
           <img class="btn-login-icon" src="../../img/google-icon.svg" alt="Google icon"/>
@@ -31,5 +32,9 @@ export const Login = () => {
   const googleButton = rootElement.querySelector('#btn-google');
   googleButton.addEventListener('click', googleLogin);
 
+  const loginButton = rootElement.querySelector('#btn-login');
+  loginButton.addEventListener('click', emailAndPasswordLogin);
+
   return rootElement;
 };
+
--- src/services/index.js
+++ src/services/index.js
@@ -10,6 +10,32 @@ export const googleLogin = () => {
     .catch();
 };
 
+export const emailAndPasswordLogin = (event) => {
+  event.preventDefault();
+  const email = document.querySelector('#email-login').value;
+  const password = document.querySelector('#password-login').value;
+ 
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((user) => {
+    console.log('usuário', user);
+    alert('usuário logado!');
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    if (errorCode === 'auth/invalid-email') {
+      alert('Email address is not valid');
+    } else if (errorCode === 'auth/user-disabled.') {
+      alert ('User corresponding to the given email has been disabled.');
+    } else if (errorCode === 'auth/user-not-found') {
+      alert ('There is no user corresponding to the given email.');
+    } else if (errorCode === 'auth/wrong-password') {
+      alert ('Password is invalid for the given email, or the account corresponding to the email does not have a password set.');
+    } else {
+      alert('Something went wrong. Please try again');
+    }
+  })
+};
+
 export const createRegister = (e) => {
   e.preventDefault();
   const email = document.getElementById('register-email').value;
