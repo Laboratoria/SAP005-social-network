5bafc393498eb04ab7b11cbce7bf57c0629fa81c
caroline costa <carolmarcionilia@gmail.com>
2021-01-04 13:56:12 -0300

    fix indentation

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -10,18 +10,18 @@ export const Login = () => {
         <h2 class="title-login">Nome do app</h2>
       </header>
       <section>
-        <form class="form-login" id="form>
-        <label class="label-enter">Entrar</label>
-        <label class="label-login">E-mail</label>
-        <input type="email" class="input-login" id="email-login" name="email-login" required>
-        <label class="label-login">Senha</label>
-        <input type="password" class="input-login" id="password-login" minlength="8" name="password-login" required>
-        <button type="submit" class="btn-login" id="btn-login">Entrar</button>
-        <label class="alternative">Ou</label>
-        <button id="btn-google">
-          <img class="btn-login-icon" src="../../img/google-icon.svg" alt="Google icon"/>
-          <span class="btn-login-text">Sign in with Google</span>
-        </button>
+        <form class="form-login">
+          <label class="label-enter">Entrar</label>
+          <label class="label-login">E-mail</label>
+          <input type="email" class="input-login" id="email-login" name="email-login" required>
+          <label class="label-login">Senha</label>
+          <input type="password" class="input-login" id="password-login" minlength="8" name="password-login" required>
+          <button type="submit" class="btn-login" id="btn-login">Entrar</button>
+          <label class="alternative">Ou</label>
+          <button id="btn-google">
+            <img class="btn-login-icon" src="../../img/google-icon.svg" alt="Google icon"/>
+            <span class="btn-login-text">Sign in with Google</span>
+          </button>
         </form>
       </section>
       <p class="register-login">
