ff85ee16a19cba52d98d08a2d3f2b970af3c4e78
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2021-01-04 10:30:14 -0300

    Initial desktop login style

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,21 +2,25 @@ export const Login = () => {
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
   <div class="main-login">
-    <img class="logo" src="/images/logo-deva.png"/>
-    <img class="image-desktop" src="/images/image-desktop.png"/>
-    <div class="mail-login">
-      <form class="login-input">
-        <input class="login-input" type="text" placeholder="Email ou nome de usuária">
-        </input>
-        <input class="login-input" type="text" placeholder="Insira sua senha">
-        </input>
-        <button class="login-input" type="submit">LOGIN</button>
-      </form>
+    <div class="login-images">
+      <img class="logo" src="/images/logo-deva.png"/>
+      <img class="image-desktop" src="/images/image-desktop.png"/>
     </div>
-    <h4>OU<h4>
-    <div class="google-github-login">
-      <button class="login-google" type="submit">Entrar com GOOGLE</button>
-      <button class="login-github" type="submit">Entrar com GITHUB</button>
+    <div class="login-data">
+      <div class="mail-login">
+        <form class="login-input">
+          <input class="login-input" type="text" placeholder="Email ou nome de usuária">
+          </input>
+          <input class="login-input" type="text" placeholder="Insira sua senha">
+          </input>
+          <button class="login-input" type="submit">LOGIN</button>
+        </form>
+      </div>
+      <h4>OU</h4>
+      <div class="google-github-login">
+        <button class="login-google" type="submit">Entrar com GOOGLE</button>
+        <button class="login-github" type="submit">Entrar com GITHUB</button>
+      </div>
     </div>
     <div class="new-register">
       <p id="without-register">Não tem uma conta?</p>
