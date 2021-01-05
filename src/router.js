7018e0e5cab352d0c06e46cef024a76849c2f4a6
Talita-8 <tlthmf1@outlook.com>
2021-01-05 16:15:14 -0300

    docs/refatoraçao do login e cadastro

--- src/router.js
+++ src/router.js
@@ -3,10 +3,9 @@ import { Login } from './pages/login/index.js';
 import { onNavigate } from './utils/history.js';
 import { Subscribe } from './pages/subscribe/index.js';
 import { Feed } from './pages/feed/index.js';
-import { authentication } from './pages/subscribe/subscribe.js';
 import { loginFeed } from './pages/login/login.js';
 
-const routeRender = () => {
+/*const routeRender = () => {
   const rootDiv = document.getElementById('root');
   const routes = {
    // '/' : Home,
@@ -22,7 +21,14 @@ const routeRender = () => {
 window.addEventListener('popstate', routeRender);
 window.addEventListener('load', () => {
  
-  
+  document
+    .getElementById('subscribe')
+    .addEventListener('click', (e) => {
+      e.preventDefault();
+      onNavigate('/subscribe');
+      document.getElementById('logo-name').style.display = "none"
+      authentication()
+    });
   routeRender();
 });
-
+*/
