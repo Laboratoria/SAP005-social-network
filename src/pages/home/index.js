98f7cf725d1264804cb7d63557e41f66bc94f782
elis-ctrl <elis_emb@hotmail.com>
2021-01-05 13:58:34 -0300

    tela home-inicial com alterações para realizar

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,40 +1,18 @@
-export const Home = () => {
-  const container = document.createElement('div');
-  container.classList.add('div-home');
-  container.innerHTML = `
-    <img alt='Logo Runners' id='logo'>
-    <h1>Olá!</h1>
-  <form class='login'>
-    <div class='input-form'>
-    <input type='email' id='email-input' placeholder='E-mail'>
-    <input type='password' id='password-home' placeholder='Senha'>
-    </div>
-    <button id='submit-home'>Entrar</button>
-    <div id="social-home">
-    <p>Logar com:</p>
-    <div class='button-social'>
-    <img src="./assets/facebook.png" alt='Logo Facebook' id='facebook-home'>
-    <img src="./assets/google.png" alt='Logo Google' id='google-home'>
-    </div>
-    </div>
-    <p>Não tem uma conta? <a href="/cadastro">Cadastre-se</a></p>
-    </form>
-    `;
-  const html = {
-    emailInput: document.getElementById('email-input'),
-    passwordHome: document.getElementById('password-home'),
-    submitHome: document.getElementById('submit-home'),
-    facebookHome: document.getElementById('facebook-home'),
-    googleHome: document.getElementById('google-home'),
-  };
-  html.submitHome.addEventListener('click', () => {
-    const email = html.emailInput;
-    const password = html.passwordHome;
-    firebase.auth().singInWithEmailAndPassword(email.value, password.value).then(() => {
-      console.log('emailautenticado');
-    }).catch(() => {
-      console.log('mensagemdeautentificaçãoerrada');
-    });
-    return container;
-  });
+export const login = {
+  signIn(email, password) {
+    return firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password);
+  },
 };
+
+export const signGoogle = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  return firebase
+    .auth()
+    .signInWithPopup(provider);
+};
+
+export const signFacebook = () => {
+
+}
