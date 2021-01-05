e547cf38a5f9ef031a64bcfd0a4ac9ebf3332fed
Nijoane <nicajoane@outlook.com>
2021-01-05 16:27:25 -0300

    adicionando serviço de login firebase

--- src/services/index.js
+++ src/services/index.js
@@ -1,15 +1,26 @@
-// import { firebaseConfig } from  "config/config.js"
-// // exporte suas funções
-
+// exporte suas funções
 export const CreateUser = (email, password) => {
   firebase
     .auth()
     .createUserWithEmailAndPassword(email, password)
     .then((response) => {
-      window.location = '#singup';
+      window.location.pathname = 'singup';
       response('Você está logado parça!');
     })
     .cath((error) => {
       error.alert('Reveja sua senha parça!');
+    });
+};
+
+export const SingIn = (email, password) => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email.trim(), password)
+    .then((response) => {
+      window.location.pathname = 'login';
+      response('Bem-vindo!');
+    })
+    .catch((error) => {
+      error.alert('Ocorreu um erro ao realizar login');
     });
 };
