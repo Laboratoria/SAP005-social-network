05cf0dbd1b586a98609d0d4b797bf246fbc7908a
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2021-01-05 14:44:12 -0300

    Added firebase config js

---
+++ src/files/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+(function () {const firebaseConfig = {
+  apiKey: "AIzaSyAlRpXgQyVUgovZRFGCXhIyN_l1fwsBo9E",
+    authDomain: "rede-social-79057.firebaseapp.com",
+    projectId: "rede-social-79057",
+    storageBucket: "rede-social-79057.appspot.com",
+    messagingSenderId: "384442606375",
+    appId: "1:384442606375:web:62b9ed1f997aee49d6ff02"
+};
+
+firebase.initializeApp(firebaseConfig);})
+()
