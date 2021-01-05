a013c009e528c9d023bd5ae7b01c3377aee4a1c0
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2021-01-05 14:32:40 -0300

    Login email function

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -9,11 +9,11 @@ export const Login = () => {
     <div class="login-data">
       <div class="mail-login">
         <form class="login-input">
-          <input class="login-input" type="text" placeholder="Email ou nome de usuária">
+          <input class="login-input" id="email-login" type="text" placeholder="Email ou nome de usuária">
           </input>
-          <input class="login-input" type="text" placeholder="Insira sua senha">
+          <input class="login-input" id="password-login" type="text" placeholder="Insira sua senha">
           </input>
-          <button class="login-input" type="submit">LOGIN</button>
+          <button class="login-input" id="lgn-btn" type="submit">LOGIN</button>
         </form>
       </div>
       <h4>OU</h4>
@@ -27,10 +27,60 @@ export const Login = () => {
       </div>
       <div class="agreement">
         <p class="without-register">ao cadastrar-se você concorda com nosso</p>
-        <a class="register" href="#">código de conduta</a>
+        <a class="register" id="sgnUp-btn" href="#">código de conduta</a>
       </div>
     </div>
   </div>
   `;
+
+const email = rootElement.querySelector("#email-login");
+const  password = rootElement.querySelector("#password-login");
+const loginButton = rootElement.querySelector("#lgn-btn");
+const signUpButton = rootElement.querySelector("#sgnUp-btn");
+
+function clear () {
+  email.value = "";
+  password.value = "";
+  
+};
+
+loginButton.addEventListener("click", (e) =>{
+  e.preventDefault();
+  firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+  clear();
+});
+
+signUpButton.addEventListener("click", (e)=>{
+  e.preventDefault();
+  rootElement.innerHTML = "";
+  
+  // firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+  // .catch((error) => {
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;
+  // });
+  // clear();
+});
+
+const logoutButton = rootElement.querySelector("#sgnOutBtn")
+logoutButton.addEventListener("click", (e)=>{
+  e.preventDefault();
+  firebase.auth().signOut();
+});
+
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+.then(function() {
+  return firebase.auth().signInWithEmailAndPassword(email.value, password.value);
+})
+.catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+});
+
   return rootElement;
 };
