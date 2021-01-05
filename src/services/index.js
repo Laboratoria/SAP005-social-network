10e3f626b5b83e9c5b6ffbe3285ab30538aa9fd3
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2021-01-05 14:21:03 -0300

    Validation function

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,16 @@
 // exporte suas funções
 
-export const myFunction = () => {
-  // seu código aqui
-  console.log('Olá mundo!');
+export const validation = (person) => {
+  firebase.auth()
+    .createUserWithEmailAndPassword(person.email, person.password)
+    .then(user => { console.log('usuario', user) })
+    .catch('lascou');
+};
+
+export const persist = (person) => {
+  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+  .then(function() {
+    return firebase.auth().signInWithEmailAndPassword(person.email, person.password);
+  })
+  .catch("setar erro");
 };
