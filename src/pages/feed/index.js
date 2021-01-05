03f2a4cf68a496fe1c55acf987554bc8cee57039
aluizada <luizanunes.b@gmail.com>
2021-01-05 19:37:53 -0300

    conexao_firestore

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -2,9 +2,51 @@ export const Feed = () => {
     // Coloque sua página
     const rootElement = document.createElement('div');
     rootElement.innerHTML = `
-     <p>Feed</p>
-    `
-  
+     <nav>
+      
+        <div class="menu-burger">
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+        </div>
+        <ul class="nav-links">
+          <li>
+          <a class="name-nav">
+            <strong class="name-menu">menu</strong></a>
+          </li>
+          <li>
+            <a href="#" id="end-btn">Sair <i class="fas fa-sign-out-alt"></i></a>
+          </li>
+        </ul>
+      </nav>
+        <main class="main">
+          <div class="newPost">
+            <div class="infoUser">
+              <div class="imgUser"></div>
+              <strong class="nameUser"></strong>
+            </div>
+            <form class="formPost" action="#" method="POST" enctype="multipart/form-data">
+              <textarea
+                name="textearea"
+                id="wrt-post"
+                placeholder="O que podemos mudar hoje com arte?"
+              ></textarea>
+              <div class="iconButtons">
+                <button id="btn-pst" type="submit" class="btnSubmit">Publicar</button>
+              </div>
+            </form>
+          </div>
+        <main>
+          <div id="all-posts"></div>
+      `;
+
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+            divFeed.querySelector('.nameUser').innerHTML = firebase.auth().currentUser.displayName;
+            divFeed.querySelector('.name-menu').innerHTML = firebase.auth().currentUser.displayName;
+        }
+    })
+
+
     return rootElement;
-  };
-  
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,68 +1,73 @@
+export const createUser = (email, password, name, lastName, talents = "cantar") => {
+    const uid = firebase.auth().currentUser.uid
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then((user) => {
+            firebase
+                .firestore()
+                .collection("Usuarios")
+                .doc(uid)
+                .set({ name, lastName, talents })
 
 
-export const createUser = (email,password, name,lastName) => {
-  firebase
-  .auth()
-  .createUserWithEmailAndPassword(email,password)
-  .then((user) => { 
-    console.log(user, "usuário criado")
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log("deu ruim", errorMessage,errorCode)
-  });
+
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log("deu ruim", errorMessage, errorCode)
+        });
 };
 
 export const userLogin = (email, password) => {
-  firebase
-  .auth()
-  .signInWithEmailAndPassword(email, password)
-  .then((result) => {
-    const token = result.user.uid;
-    const user = result.user;
-    console.log(user, token, "usuário Logado com sucesso")
-    return (token, user);
-  })
-  .catch((error) => {
-    let errorCode = error.code;
-    let errorMessage = error.message;
-    console.log("deu ruim", errorMessage, errorCode)
-    if (errorCode === "auth/wrong-password" || errorCode === 'auth/invalid-email') {
-      return alert('Email ou Senha incorreta!');
-    } 
-    return `Codigo de error: ${errorCode}`;
-  });
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then((result) => {
+            const token = result.user.uid;
+            const user = result.user;
+            console.log(user, token, "usuário Logado com sucesso")
+            return (token, user);
+        })
+        .catch((error) => {
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            console.log("deu ruim", errorMessage, errorCode)
+            if (errorCode === "auth/wrong-password" || errorCode === 'auth/invalid-email') {
+                return alert('Email ou Senha incorreta!');
+            }
+            return `Codigo de error: ${errorCode}`;
+        });
 };
 
 export const gmailLogin = () => {
-  let provider = new firebase.auth.GoogleAuthProvider();
-   firebase.auth().signInWithPopup(provider)
-   .then(function(result) {
-    let token = result.credential.accessToken;
-    let user = result.user;
-    console.log(user, "Usuário Logado com o google com sucesso",token )
-    return (user,token)
-  }).catch(function(error) {
-    let errorCode = error.code;
-    let errorMessage = error.message;
-    let email = error.email;
-    let credential = error.credential;
-    console.log("Usuário não foi logado", errorCode, errorMessage, email, credential )
-  });
-  
+    let provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+        .then(function(result) {
+            let token = result.credential.accessToken;
+            let user = result.user;
+            console.log(user, "Usuário Logado com o google com sucesso", token)
+            return (user, token)
+        }).catch(function(error) {
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            let email = error.email;
+            let credential = error.credential;
+            console.log("Usuário não foi logado", errorCode, errorMessage, email, credential)
+        });
+
 };
 
 export const userLogout = () => {
-  firebase
-  .auth()
-  .signOut()
-  .then(function() {
-    console.log("usuário deslogado com sucesso")
-  }).catch(function(error) {
-    let errorCode = error.code;
-    let errorMessage = error.message;
-    console.log("deu ruim, usuário continua logado", errorMessage, errorCode)
-  });
-  };
-
+    firebase
+        .auth()
+        .signOut()
+        .then(function() {
+            console.log("usuário deslogado com sucesso")
+        }).catch(function(error) {
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            console.log("deu ruim, usuário continua logado", errorMessage, errorCode)
+        });
+};
