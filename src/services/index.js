6fff41fa0e66cdad1b061158701e3c30a0642df0
cbalieiro <bmoliveira.camila@gmail.com>
2021-01-06 19:38:01 -0300

    Ajustes nas funções que criam usuario no banco e criação de erros

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,5 @@
-// exporte suas funções
+import { onNavigate } from '../utils/history.js';
+import { errors } from './errors.js';
 
 export const validation = (person) => {
   firebase
@@ -14,9 +15,7 @@ export const persist = (person) => {
   firebase
     .auth()
     .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-    .then(() =>
-      firebase.auth().signInWithEmailAndPassword(person.email, person.passwor),
-    )
+    .then(() => firebase.auth().signInWithEmailAndPassword(person.email, person.passwor))
     .catch('setar erro');
 };
 
@@ -24,16 +23,16 @@ export const createUser = (person) => {
   firebase
     .auth()
     .createUserWithEmailAndPassword(person.email, person.password)
-    .then(function () {
-      firebase.auth().currentUser.updateProfile({
-        displayName: person.name,
-      });
-      firebase.firestore().collection("users").doc(firebase.auth().currentUser.uid).set(person);
-    })
-    .then(function() {
-      alert("Document successfully written!");
-  })
-  .catch(function(error) {
-      console.error("Error writing document: ", error);
-  });
+
+    .then(
+      console.log(person.name),
+      () => firebase.auth().currentUser.updateProfile({ displayName: person.name }),
+      () => firebase
+        .firestore()
+        .collection('users')
+        .doc(firebase.auth().currentUser.uid)
+        .set(person),
+    )
+    .then(onNavigate('/'))
+    .catch((error) => errors(error));
 };
