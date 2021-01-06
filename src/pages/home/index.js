775dc5515ec449d68edb86bec00e514b569748d1
carolineshimada <caroline_shimada@hotmail.com>
2021-01-06 00:40:13 -0300

    Botao singup no home inserido e Redirecionamento para feed pos Login e botao de singup no home inserido

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,17 @@
 export const Home = () => {
   // Coloque sua página
   const rootElement = document.createElement('div');
-  rootElement.innerHTML = `
-    <h1>Olá, mundo!</h1>
+
+  const singupHtml = `
+    <h1>Welcome!</h1>
+    <button class="btn-singup"> SingUp </button>  
+
   `;
+  rootElement.innerHTML = singupHtml;
+  const singupButtonHtml = rootElement.querySelector('.btn-singup');
+  singupButtonHtml.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.pathname = 'singup';
+  });
   return rootElement;
 };
