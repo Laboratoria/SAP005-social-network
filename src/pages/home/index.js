bdf63211880e7e2c920838a57d9668c5f064a125
Rebeca Canesin <rebecafguilherme@gmail.com>
2021-01-07 10:47:26 -0300

    posts no banco de dados e na tela

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,7 +5,13 @@ export const Home = () => {
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
       <button class='logOut' id='sgnOutBtn' type="submit">Logout</button>
-      <h1>Olá, mundo!</h1>
+     
+      <form id="post-form">
+        <input type="text" id="post-text" width="100px" height="60px"></input>
+        <button type="submit">Publicar</button>
+      </form>
+      <ul id="post-list"></ul>
+
   `;
 
   const logoutButton = rootElement.querySelector('#sgnOutBtn')
@@ -14,5 +20,40 @@ export const Home = () => {
    logOut();
   });
 
+  const post = rootElement.querySelector('#post-form');
+  post.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const text = rootElement.querySelector('#post-text').value;
+    const post = {
+      text: text,
+      userId: 'teste',
+      likes: 'teste2',
+      comments: 'teste3',
+    }
+    const postCollection = firebase.firestore().collection('posts')
+    postCollection.add(post)
+    loadPosts();
+  });
+
+
+  function addPost (post) {
+    const template = `
+      <li id='${post.id}'>
+        ${post.data().text}
+        ${post.data().likes}
+    `
+    rootElement.querySelector('#post-list').innerHTML += template;
+  }
+
+  function loadPosts(){
+    const postCollection = firebase.firestore().collection('posts')
+    postCollection.get().then(x =>{
+      x.forEach(post => {
+        addPost(post)
+      });
+    })
+  }
+  
+  
   return rootElement;
 };
