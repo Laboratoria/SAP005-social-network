08d3a7444120a0d2110985557530769803f93b3c
Nijoane <nicajoane@outlook.com>
2021-01-08 16:58:06 -0300

    [feed] button de logout funcional

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -1,14 +1,16 @@
+import { SignOut } from '../../services/index.js';
+
 export const Feed = () => {
   // Coloque sua página
   const rootElement = document.createElement('div');
-  
-  const postHtml= `
+  const postHtml = `
     <div>
       <h1 class="feed">Account created successfully! Feed!</h1>
       <form class="form-post" id="form-post">
         <input class="text-post" id="text-post" type="textarea">
         <button class="btn-submit" id="btn-submit" type="submit">Submit</button>
       </form>
+      <button class="btn-logout">LogOut</button>
     </div> `;
 
   rootElement.innerHTML = postHtml;
@@ -19,17 +21,19 @@ export const Feed = () => {
     const textPost = document.querySelector('.text-post').value;
     const post = {
       text: textPost,
-      user_id: "maria",
+      user_id: 'maria',
       likes: 0,
-      comments: []
-    }
-    const collectionPosts = firebase.firestore().collection("posts")
+      comments: [],
+    };
+    const collectionPosts = firebase.firestore().collection('posts');
 
-    collectionPosts.add(post)
-    
-  })
+    collectionPosts.add(post);
+  });
+
+  const logOut = rootElement.querySelector('.btn-logout');
+  logOut.addEventListener('click', () => {
+    SignOut();
+  });
+
   return rootElement;
-
 };
-
-
