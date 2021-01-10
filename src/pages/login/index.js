9278a1ae7356dc0fbc51601abf4a2c7ce681320d
Sara Viana <saravianac@outlook.com>
2021-01-10 17:00:42 -0300

    CODE: SingUP/Login Finalizados estrutura e estilizacao

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -3,26 +3,31 @@ import { onNavigate } from '../../utils/history.js';
 
 export const Login = () => {
   const rootElement = document.createElement('div');
+  rootElement.setAttribute('class', 'box');
   rootElement.innerHTML = `
+  <div class='box-item'>
+  <h1><img src='../../img/logo.png' alt='Logotype' class='icon-large'></h1>
+  <img src='../../img/pessoas.png' alt='Logotype' class='icon-large'>
 
-  <div class='box-item1'>
-  <img src='./img/pessoas.png' alt='Logotype' class='icon-large' />
 </div>
 
 <div class='box-item'>
-  <img src='./img/security.png' alt='Logotype' class='icon-logohome' />
+  <h3>WELCOME TO SECURITY PET</h3>
+
   <form method='post'>
-    <input type='email' placeholder='Email' id='emailArea' class='login-area'>
+    <input type=' email' placeholder='Email' id='emailArea' class='login-area'>
     <input type='password' placeholder='Senha' id='passwordArea' class='login-area'>
   </form>
   <div class="inerror-message" id="error-login"> </div>
   <button class='button-area btn signIn'>Entrar</button>
   <p class="ou">━━━━━━━━━ OU ━━━━━━━━━</p>
-  <button class='button-area btn btnGoogle'> Acessar via <img src='../../img/google-icon.png /' alt='Google'
-      class='google-icon'>
-  </button>
+  <button class='button-area btn btnGoogle'>Acesse com <img src='./img/google-icon.png' alt='Google'
+      class='google-icon'></button>
   <p class='font-small'>Se não tem um conta, <a href='/#signup' style="color:black;font-weight: 600"
-      id='sign-up-login'>REGISTRE-SE.</a></p>
+      id='sign-up-login'>REGISTRE-SE.</a>
+  </p>
+</div>
+      
 
   `;
 
--- src/pages/singnUp/index.js
+++ src/pages/singnUp/index.js
@@ -3,21 +3,25 @@ import { onNavigate } from '../../utils/history.js';
 
 export const SignUp = () => {
   const rootElement = document.createElement('div');
+  rootElement.setAttribute('class', 'box');
   rootElement.innerHTML = `
-<div class='box-item1'>
+  <div class='box-item'>
+  <h1><img src='../../img/logo.png' alt='Logotype' class='icon-large'></h1>
   <img src='../../img/pessoas.png' alt='Logotype' class='icon-large'>
 </div>
+
 <div class='box-item'>
-  <img src='../../img/security.png' alt='Logotype' class='icon-logohome' />
   <h3>CRIAR UMA NOVA CONTA</h3>
   <form method='post'>
     <input type="text" placeholder="Insira seu nome" id="username" class='login-area'>
     <input type="email" placeholder="Insira um email" id="register-email" class="login-area">
     <input type="password" placeholder="Insira uma senha" id="register-password" class="login-area">
-    <div class="error-message" id="singup-error"></div>
+    <div class="error-message" id="singup-error">
+    </div>
     <button class="btn button-area" id="button-register">Cadastre-se</button>
   </form>
 </div>
+
 `;
 
   const email = rootElement.querySelector('#register-email');
