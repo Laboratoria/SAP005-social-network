43ace2fea18e72a6831097b63da225df0e122ed0
Mairã Soares <71467930+MairaSoares@users.noreply.github.com>
2021-01-09 22:49:59 -0300

    alteraçõesMaicadastro

--- src/home.js
+++ src/home.js
@@ -6,18 +6,20 @@ export const Home = () => {
   
         <div id='logo-name' class='network-name'>
           <h1 class='tpm-name'>TPM</h1>
-          <p id='subtitle-one'>Tudo é possível para Maes</p>
+          <fieldset>
+            <p id='subtitle-one'>Tudo é possível para Maes</p>
 
-          <img class="mae-carol" src="./img/mcarol.jpg" alt="Desenho de Mae Carol Jemison astronauta">
+            <img class="mae-carol" src="./img/mcarol.jpg" alt="Desenho de Mae Carol Jemison astronauta">
 
-          <p>Essa é uma rede destinada a você mulher que pretende confiar mais
-            em si mesma para ingressar na área que você sempre sonhou!  <br><br>
+            <p>Essa é uma rede destinada a você mulher que pretende confiar mais
+              em si mesma para ingressar na área que você sempre sonhou!  <br><br>
             
-            Aqui você pode compartilhar ideias, oportunidades, cursos e qualquer
-            outra contribuição para várias outras mulheres, formando uma rede de apoio
-            em um lugar seguro.  <br><br>
+              Aqui você pode compartilhar ideias, oportunidades, cursos e qualquer
+             outra contribuição para várias outras mulheres, formando uma rede de apoio
+              em um lugar seguro.  <br><br>
       
-            Afinal tudo é possível para Maes!  <br> </p>
+              Afinal tudo é possível para Maes!  <br> 
+            </p>
       
            <p> Mae Carol Jemison é uma médica, engenheira e ex-astronauta estadunidense. 
             Foi a primeira mulher negra a ir para o espaço, quando serviu como especialista 
@@ -26,7 +28,8 @@ export const Home = () => {
       
             Mae foi tudo o que ela desejou ser, e você? O que deseja? 
             Então você vai conseguir!
-          </p>
+            </p>
+          </fieldset>
         </div>
       `
 
