8f97f5a0081a196dbd5ac50011cc0321520976ec
Dunia <dunia07@hotmail.com>
2021-01-11 18:42:38 -0300

    feat(load-post): carrega o post na tela de feed (incompleto)

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -1,3 +1,5 @@
+// import { template } from "@babel/core";
+
 export const Feed = () => {
   // Coloque sua página
   const rootElement = document.createElement('div');
@@ -9,10 +11,14 @@ export const Feed = () => {
         <input class="text-post" id="text-post" type="textarea">
         <button class="btn-submit" id="btn-submit" type="submit">Submit</button>
       </form>
+      <div class="load-posts" id="load-posts">
+      </div>
     </div>
   `;
 
   rootElement.innerHTML = postHtml;
+
+  const loadPosts = rootElement.querySelector('.load-posts');
 
   const createPost = rootElement.querySelector('.form-post');
   createPost.addEventListener('click', (e) => {
@@ -20,7 +26,7 @@ export const Feed = () => {
     const textPost = document.querySelector('.text-post').value;
     const post = {
       text: textPost,
-      user_id: "maria",
+      user_id: `${firebase.auth().currentUser.email}`,
       likes: 0,
       comments: []
     }
@@ -29,8 +35,50 @@ export const Feed = () => {
     collectionPosts.add(post)
     
   })
-  return rootElement;
+  
+  //VERSÃO KARINA
+  function showPosts (data) {
+    const templatePosts = `
+      <div>
+      <p> ${data.post} </div>
+      </div>
+    `
 
+    loadPosts.innerHTML += templatePosts
+  }
+
+  firebase.firestore().collection(posts)/*.orderBy("date", "desc")*/.onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach((post) => {
+      if (post.type ==="added") {
+        showPosts(post.doc.data.doc.id)
+      }
+    })
+  })
+  
+  //VERSÃO DANIEL
+  // function addPost(post) {
+  //   const templatePost = `
+  //     <li id="${post.id}">
+  //       ${post.data().text} ❤️ ${post.data().likes}
+  //     </li>
+  //   `
+  //   document.querySelector('.load-posts').innerHTML += templatePost;
+  // }
+
+  // function loadPosts() {
+  //   const collectionPosts = firebase.firestore().collection("posts")
+  //   document.querySelector('.load-posts').innerHTML = "Carregando..."
+  //   collectionPosts.get().then(snap => {
+  //     document.querySelector('.load-posts').innerHTML = " "
+  //     snap.forEach(post => {
+  //       addPost(data)
+  //     })
+  //   })
+  //   return loadPosts();
+  // }
+
+  return rootElement;
+  
 };
 
 
