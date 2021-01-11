ac7f780479a25609c0b4c28441f7c00e65ff2349
Sara Viana <saravianac@outlook.com>
2021-01-11 17:35:54 -0300

    CODE: Firebaseconf funcionando

--- src/pages/singnUp/index.js
+++ src/pages/singnUp/index.js
@@ -33,8 +33,8 @@ export const SignUp = () => {
   registerButton.addEventListener('click', () => {
     console.log('oi');
     firebase.auth().createUserWithEmailAndPassword(emailButton.value, passwordButton.value);
-    console.log(emailButton.value);
-    console.log(passwordButton.value);
+    // console.log(emailButton.value);
+    // console.log(passwordButton.value);
   });
 
   const linkLogin = rootElement.querySelector('#button-login');
