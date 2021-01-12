0ed5b20d27ac08ed3476652f4dcd9c5f2c45b6a5
JulianaAmoriN <juamorim.sn@gmail.com>
2021-01-12 20:32:58 -0300

    collection post tentativa

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -3,14 +3,14 @@ import {
 } from "/services/index.js"
 
 export const Feed = () => {
-  const nav = Navigation();
+const nav = Navigation();
 
-  const rootElement = document.createElement('div');
-  rootElement.appendChild(nav);
+const rootElement = document.createElement('div');
+rootElement.appendChild(nav);
 
-  const content = () => {
-    const contentElement = document.createElement('div');
-    contentElement.innerHTML = `
+const content = () => {
+  const contentElement = document.createElement('div');
+  contentElement.innerHTML = `
     <h1>Post</h1>
     <form>
       <textarea name="" id="textPost" cols="70" rows="5" placeholder="escreva aqui..."></textarea>
@@ -20,57 +20,49 @@ export const Feed = () => {
       <p id="outputPost"></p>
     </div>
     `;
-    return contentElement;
-  };
-  rootElement.appendChild(content());
+  return contentElement;
+};
+rootElement.appendChild(content());
 
-  const textPost = rootElement.querySelector("#textPost");
-  const creatPost = rootElement.querySelector("#creatPost");
+const textPost = rootElement.querySelector("#textPost");
+const creatPost = rootElement.querySelector("#creatPost");
 
-  const outputPost = rootElement.querySelector("#outputPost");
+const outputPost = rootElement.querySelector("#outputPost");
 
-  creatPost.addEventListener("click", (event) => {
-    event.preventDefault();
-    const saveTextPost = textPost.value;
-    console.log("post: " + saveTextPost);
+creatPost.addEventListener("click", (event) => {
+  event.preventDefault();
+  const saveTextPost = textPost.value;
 
-    const user = firebase.auth().currentUser;
-    const uid = user.uid
-    const docFirestore = firebase.firestore();
-    const docRef = docFirestore.doc(`/post/${uid}`);
+  const user = firebase.auth().currentUser;
+  const docFirestore = firebase.firestore();
 
-    docRef.set({
-      name: user.displayName,
-      text: saveTextPost,
-      date: (new Date()).toLocaleString(),
-    }).then(() => {
-      docRef.get().then(function (doc) {
-        if (doc && doc.exists) {
-          const myData = doc.data();
-          outputPost.innerHTML = `${myData.text} <br> ${myData.date} <br> ${ myData.name}`;
-        }
-      }).cath((error) => {
-        console.log("oh no!", error)
-      })
-    })
+  docFirestore.collection(`post`).add({
+    name: user.displayName,
+    text: saveTextPost,
+    date: (new Date()).toLocaleString(),
+    uid: user.uid,
+  })
 
+//   const uid = user.uid
+//   const docFirestore = firebase.firestore();
+//   const docRef = docFirestore.doc(`/post/${uid}`);
 
+//   docRef.set({
+//     name: user.displayName,
+//     text: saveTextPost,  
+//     date: (new Date()).toLocaleString(),
+// }).then(() => {
+//   docRef.get().then(function (doc) {
+//     if (doc && doc.exists) {
+//       const myData = doc.data();
+//       outputPost.innerHTML = `${myData.text} <br> ${myData.date} <br> ${ myData.name}`;
+//     }
+//   }).cath((error) => {
+//     console.log("oh no!", error)
+//   })
+// })
 
-    // docRef.set({
-    //  name: firebase.auth().currentUser.displayName,
-    //   textPost: saveTextPost
-    // }).then(() => {
-    //   docRef.get().then(function(doc){
-    //     if(doc && doc.exists){
-    //       const myData = doc.data();
-    //       outputPost.innerHTML = myData.textPost;
-    //     }
-    //   }).cath((error) => {
-    //     console.log("oh no!", error)
-    //   })
-    // })
+});
 
-  });
-
-  return rootElement;
+return rootElement;
 };
