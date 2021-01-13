0f053011182368557428272a9188fa5eecdc1a0a
Mairã Soares <71467930+MairaSoares@users.noreply.github.com>
2021-01-13 18:44:20 -0300

    lógica e estilização do botão login google

--- src/pages/inicial-page/login.js
+++ src/pages/inicial-page/login.js
@@ -35,6 +35,9 @@ document.getElementById('main-page').innerHTML = `
       </fieldset>
     </form>
   </div>
+  <div>
+       <input class= "config-inputs" type= "button" value= "Google"/>
+  </div>//
 
   <section id='login-button' class="half-screen">
     <button id="login-btn">Login</button>
@@ -91,6 +94,9 @@ document.getElementById('main-page').innerHTML = `
       let email = document.getElementById('email').value;
       let password = document.getElementById ('password').value;
       emailLogin(email, password);
-      })
     })
+  })
+
+
+
 }
