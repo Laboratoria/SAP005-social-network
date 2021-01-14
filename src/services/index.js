fb5132cc30e797daa4b7cc6ec385e31b4b584186
Talita-8 <tlthmf1@outlook.com>
2021-01-14 01:42:42 -0300

    Funçoes de like e love funcionando

--- src/services/index.js
+++ src/services/index.js
@@ -77,8 +77,6 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
  }) 
 }
-const dataStorage = dataBase.collection('users')
-
 // FUNÇÕES FIREBASE -> FEED
 export const Post = () => {
 
@@ -86,16 +84,14 @@ export const Post = () => {
      e.preventDefault();
   
      let postText = document.getElementById('write-post').value
-     let time = new Date()
-        dataBase.collection('users').doc(`${time}`).set({
+        
+        dataBase.collection('Posts').add({
         post_text: postText,
+        date: new Date(),
         id_user: firebase.auth().currentUser.uid,
         username: firebase.auth().currentUser.displayName,
         likes: 0,
-        loveIt: 0,
-        funny: 0,
-        congratulations: 0,
-        date: new Date()
+        loveIt: 0
       })
       .then(function() {
         console.log("Post enviado com sucesso!");
@@ -106,21 +102,23 @@ export const Post = () => {
       });
       document.getElementById('write-post').value = " "
     })
-
   } 
-  let docRef = dataStorage.doc('posts')
  
   export const getPosts = () => {
-    const post = dataBase.collection('users').orderBy("date", "desc")
+    const post = dataBase.collection('Posts').orderBy("date", "desc")
     return post.get()
     };
   
-  export const Like = (time) => {
-    dataBase.collection('users').doc(`${time}`).update({
-    likes: firebase.firestore.FieldValue.increment(50)
+  export const Like = (id) => {
+    dataBase.collection('Posts').doc(`${id}`).update({
+    likes: firebase.firestore.FieldValue.increment(1)
     })
   }
-
+  export const Love = (id) => {
+    dataBase.collection('Posts').doc(`${id}`).update({
+    loveIt: firebase.firestore.FieldValue.increment(1)
+    })
+  }
   
   export const logOut = () => {
            
@@ -130,66 +128,6 @@ export const Post = () => {
         document.getElementById('root').innerHTML= " ";
         document.getElementById('main-page').style.display = "block";           
         })
-       }  }
+       }  
+      }
    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access Cloud Firestore.
-const admin = require('firebase-admin');
-admin.initializeApp();
-
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Cloud Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Cloud Firestore using the Firebase Admin SDK.
-  const writeResult = await admin.firestore().collection('messages').add({original: original});
-  // Send back a message that we've succesfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
-});
-
-//export const myFunction = () => {
-  // seu código aqui
-//  console.log('Olá mundo!');
-//};
-
-// Listens for new messages added to /messages/:documentId/original and creates an
-// uppercase version of the message to /messages/:documentId/uppercase
-exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
-    .onCreate((snap, context) => {
-      // Grab the current value of what was written to Cloud Firestore.
-      const original = snap.data().original;
-
-      // Access the parameter `{documentId}` with `context.params`
-      functions.logger.log('Uppercasing', context.params.documentId, original);
-
-      const uppercase = original.toUpperCase();
-
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to Cloud Firestore.
-      // Setting an 'uppercase' field in Cloud Firestore document returns a Promise.
-      return snap.ref.set({uppercase}, {merge: true});
-    });
-
-*/
